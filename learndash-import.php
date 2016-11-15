aed3989b1990f9f01c3caa74276ed65626e7ba4c
Brandon Tassone <brandontassone@gmail.com>
2016-11-15 12:29:42 -0500

    Removed verbose course import info. Replaced with more simple interface / data showing. Can make it more complex in the future but hidden away

--- learndash-import.php
+++ learndash-import.php
@@ -38,41 +38,19 @@ function learndash_import_menu_page() {
 
     ?>
     <style>
-        .course { margin-bottom: 60px; }
-        .course:last-child { margin-bottom: 0; }
-        .title-wrap, .sub-title-wrap { padding: 10px; box-sizing: border-box; }
+        .title-wrap { padding: 10px; box-sizing: border-box; }
         .title-wrap { background: #1e8cbe; color: #fff; }
         .title-wrap.delete-data { margin-bottom: 20px; }
-        .sub-title-wrap { border: 1px solid #1e8cbe; border-top: none; }
 
-        .heading-title, .sub-heading-title { text-transform: uppercase; margin: 0; padding: 0; }
-        .title-wrap .heading-title, .title-wrap .sub-heading-title { margin: 0; padding: 0; }
+        .heading-title { text-transform: uppercase; margin: 0; padding: 0; }
+        .title-wrap .heading-title { margin: 0; padding: 0; }
         .heading-title { color: #fff; }
-        .sub-heading-title { color: #222; }
 
         .heading-wrap { margin-bottom: 20px; }
 
-        .quizzes-container { padding: 10px; box-sizing: border-box; border: 1px solid #1e8cbe; }
-
-        .vert-center-me { display: flex; align-items: center; }
-
         .quiz-info-item label { width: 125px; text-transform: uppercase; font-weight: 700; }
 
-        .quiz-container { margin-bottom: 30px; }
-        .quiz-heading { background: orange; padding: 10px; color: #fff;  }
-        .quiz-container:last-child { margin-bottom: 0; padding-bottom: 0; border-bottom: none; }
-        .questions-container { border: 1px solid orange; padding: 20px 10px 10px 10px; border-top: none; box-sizing: border-box; }
-
-        .question-container { margin-bottom: 20px; }
-        .question-container:last-child { margin-bottom: 0; }
-
         .question-info-item label { font-weight: 700; text-transform: uppercase; width: 125px; }
-        .question-heading { background: #1e8bce; padding: 10px; color: #fff; }
-        .question-possible-answers { padding: 10px; border: 1px solid #1e8cbe; border-top: none; }
-
-        .possible-answer-container { background: orangered; color: #fff; margin-bottom: 10px; padding: 10px; box-sizing: border-box; }
-        .possible-answer-container.correct { background: limegreen; }
-        .possible-answer-container:last-child { margin-bottom: 0; }
 
         .rows-removed-container { background: #ddd; padding: 10px; box-sizing: border-box; margin-bottom: 20px; }
 
@@ -87,6 +65,18 @@ function learndash_import_menu_page() {
         .main-action-wrap button:hover { background: darkorange; }
 
         .json-structure { background: #ddd; padding: 10px; box-sizing: border-box; }
+
+        .courses-import-container { display: flex; }
+
+        .course { background: orange; color: #fff; padding: 10px; box-sizing: border-box; margin-bottom: 20px; display: inline-block; margin-right: 20px; }
+
+        .course .title-row { text-transform: uppercase; font-size: 1.25em; display: flex; align-items: center; margin-bottom: 10px; }
+        .course .title-row label { margin-right: 6px; font-weight: 500; }
+
+        .course .information-row { background: #f1f1f1; color: #222; }
+        .course .information-row div { display: flex; align-items: center; padding: 10px; }
+        .course .information-row div:nth-child(even) { background: #fff; }
+        .course .information-row div label { width: 250px; font-weight: 500; text-transform: uppercase; border-right: 1px solid #222; padding-right: 10px; margin-right: 10px; }
 
         #go-back-to-main { float: right; }
 
@@ -114,93 +104,91 @@ function learndash_import_menu_page() {
 
     if($run) {
 
+        ?>
+        <div class="heading-wrap">
+            <div class="title-wrap">
+                <h1 class="heading-title">Importing Data</h1>
+            </div>
+        </div>
+        <div class="courses-import-container">
+        <?php
+
         foreach($data as $course):
             $course_id = $course->course_id;
             $course_title = $course->course_title;
 
             $wp_course_id = learndash_import_create_course($course_id, $course_title);
+
+            $quiz_count = 0;
+            $question_count = 0;
+            $answer_count = 0;
             ?>
             <div class="course">
-                <div class="heading-wrap">
-                    <div class="title-wrap">
-                        <h1 class="heading-title"><?php echo $course_title; ?></h1>
-                    </div>
-                    <div class="sub-title-wrap">
-                        <h6 class="sub-heading-title"><?php echo $course_id; ?></h6>
-                    </div>
-                </div>
-                <div class="quizzes-container">
-            <?php
-            $quiz_prereq_info = array();
+                <?php
+                    $quiz_prereq_info = array();
 
-            foreach($course->quizzes as $quiz):
-                $quiz_id = $quiz->quiz_id;
-                $quiz_title = $quiz->quiz_title;
-                $prereq_quiz_id = $quiz->prereq_quiz_id ?: 'NULL';
+                    foreach($course->quizzes as $quiz):
+                        $quiz_id = $quiz->quiz_id;
+                        $quiz_title = $quiz->quiz_title;
+                        $prereq_quiz_id = $quiz->prereq_quiz_id ?: 'NULL';
 
-                $prereq_info = learndash_import_create_quiz($quiz_id, $quiz_title, $wp_course_id, $prereq_quiz_id);
-                $quiz_prereq_info[] = $prereq_info;
-                ?>
-                <div class="quiz-container">
-                    <div class="quiz-heading">
-                        <div class="quiz-info-item vert-center-me"><label>PreReq Quiz Id: </label><?php echo $prereq_quiz_id; ?></div>
-                        <div class="quiz-info-item vert-center-me"><label>Quiz Id: </label><?php echo $quiz_id; ?></div>
-                        <div class="quiz-info-item vert-center-me"><label>Quiz Title: </label><?php echo $quiz_title; ?></div>
-                    </div>
-                    <div class="questions-container">
-                    <?php
+                        $prereq_info = learndash_import_create_quiz($quiz_id, $quiz_title, $wp_course_id, $prereq_quiz_id);
+                        $quiz_prereq_info[] = $prereq_info;
 
-                    foreach($quiz->questions as $question):
-                        $question_id = $question->question_id;
-                        $question_text = $question->question_text;
-                        $possible_answers = $question->possible_answers;
+                        foreach($quiz->questions as $question):
+                            $question_id = $question->question_id;
+                            $question_text = $question->question_text;
+                            $possible_answers = $question->possible_answers;
 
-                        learndash_import_create_question($prereq_info["quiz_id_master"], $question_text, $possible_answers);
-                        ?>
-                        <div class="question-container">
-                            <div class="question-heading">
-                                <div class="question-info-item vert-center-me"><label>Question Id: </label><?php echo $question_id; ?></div>
-                                <div class="question-info-item vert-center-me"><label>Question Text: </label><?php echo $question_text; ?></div>
-                            </div>
-                            <div class="question-possible-answers">
-                                <?php
-                                foreach($possible_answers as $possible_answer):
-                                    $answer_id = $possible_answer->answer_id;
-                                    $answer_text = $possible_answer->answer_text;
-                                    $correct = $possible_answer->correct;
-                                    ?>
-                                    <div class="possible-answer-container <?php echo $correct ? 'correct' : ''; ?>">
-                                        <div class="possible-answer-item vert-center-me"><label>Answer Id: </label><?php echo $answer_id; ?></div>
-                                        <div class="possible-answer-item vert-center-me"><label>Answer Text: </label><?php echo $answer_text; ?></div>
-                                        <div class="possible-answer-item vert-center-me"><label>Correct: </label><?php echo $correct ? 'true' : 'false'; ?></div>
-                                    </div>
-                                    <?php
-                                endforeach;
-                                ?>
-                            </div>
-                        </div>
-                        <?php
+                            learndash_import_create_question($prereq_info["quiz_id_master"], $question_text, $possible_answers);
+
+                            foreach($possible_answers as $possible_answer):
+                                $answer_id = $possible_answer->answer_id;
+                                $answer_text = $possible_answer->answer_text;
+                                $correct = $possible_answer->correct;
+
+                                $answer_count++;
+                            endforeach;
+
+                            $question_count++;
+                        endforeach;
+
+                        $quiz_count++;
                     endforeach;
 
-                    ?>
+                    // Update the recently imported prerequisites
+                    learndash_import_set_prerequisites($quiz_prereq_info);
+                ?>
+                <div class="title-row">
+                    <label>Course: </label>
+                    <span><?php echo $course_title; ?></span>
+                </div>
+                <div class="information-row">
+                    <div>
+                        <label>Number of quizzes imported</label>
+                        <span><?php echo $quiz_count; ?></span>
+                    </div>
+                    <div>
+                        <label>Number of questions imported</label>
+                        <span><?php echo $question_count; ?></span>
+                    </div>
+                    <div>
+                        <label>Number of answers imported</label>
+                        <span><?php echo $answer_count; ?></span>
                     </div>
                 </div>
-                <?php
-            endforeach;
-
-            // Update the recently imported prerequisites
-            learndash_import_set_prerequisites($quiz_prereq_info);
-            ?>
-            </div>
             </div>
             <?php
         endforeach;
+        ?>
+        </div>
+        <?php
     }
 
     if($delete) {
         ?>
         <div class="title-wrap delete-data">
-            <h1 class="heading-title">Remove All Data</h1>
+            <h1 class="heading-title">Removing All LearnDash Data</h1>
         </div>
         <?php
         $passed = learndash_import_delete_all_data();
